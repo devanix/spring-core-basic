f2456257ef1540b2e07f7449cd00dd7a23a7a607
mjkim <mjkim@pentasecurity.com>
2022-11-20 17:19:09 +0900

    세션2. 주문과 할인 정책 테스트

---
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,29 @@
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    @DisplayName("맴버 생성 및 주문 할인 가격 검증")
+    void createOrder() {
+        // 멤버 가입
+        long memberId = 1L;
+        Member member = new Member(memberId, "devanix", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "ItemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
