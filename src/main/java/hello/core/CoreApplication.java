d5445eb4e87eb704b28bb2fd18ae0d4f4e1ee189
mjkim <mjkim@pentasecurity.com>
2022-11-20 18:36:48 +0900

    세션3. 스프링으로 전환하기

--- src/main/java/hello/core/CoreApplication.java
+++ src/main/java/hello/core/CoreApplication.java
@@ -1,12 +1,29 @@
 package hello.core;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import hello.core.config.AppConfig;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-@SpringBootApplication
+import java.util.Arrays;
+
 public class CoreApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CoreApplication.class, args);
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // 등록된 빈 전부 출력
+        String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
+        for (String definitionName : beanDefinitionNames) {
+            Object bean = applicationContext.getBean(definitionName);
+            System.out.println("bean = " + bean);
+        }
+
+        // 등록된 빈의 개수 출력
+        int beanDefinitionCount = applicationContext.getBeanDefinitionCount();
+        System.out.println("beanDefinitionCount = " + beanDefinitionCount);
+
     }
 }
