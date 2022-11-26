3792785027ed5ac81d8ccf1e9a9dca36cae2fc85
mjkim <mjkim@pentasecurity.com>
2022-11-26 15:55:50 +0900

    세션4. 컨테이너에 등록된 모든 빈 조회

--- src/main/java/hello/core/CoreApplication.java
+++ src/main/java/hello/core/CoreApplication.java
@@ -1,18 +1,14 @@
 package hello.core;
 
 import hello.core.config.AppConfig;
-import hello.core.member.Grade;
-import hello.core.member.Member;
-import hello.core.member.MemberService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import java.util.Arrays;
 
 public class CoreApplication {
 
     public static void main(String[] args) {
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
 
         // 등록된 빈 전부 출력
         String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
