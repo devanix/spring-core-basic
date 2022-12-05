36e863c1f70e81a862b1b255037208dbd65da898
mjkim <mjkim@pentasecurity.com>
2022-12-06 00:36:08 +0900

    세션9. Request 스코프 예제 만들기 - 웹 환경 추가

--- src/main/java/hello/core/CoreApplication.java
+++ src/main/java/hello/core/CoreApplication.java
@@ -1,25 +1,12 @@
 package hello.core;
 
-import hello.core.config.AppConfig;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class CoreApplication {
 
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-
-        // 등록된 빈 전부 출력
-        String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
-        for (String definitionName : beanDefinitionNames) {
-            Object bean = applicationContext.getBean(definitionName);
-            System.out.println("bean = " + bean);
-        }
-
-        // 등록된 빈의 개수 출력
-        int beanDefinitionCount = applicationContext.getBeanDefinitionCount();
-        System.out.println("beanDefinitionCount = " + beanDefinitionCount);
-
+        SpringApplication.run(CoreApplication.class, args);
     }
 }
